1439237654e67f2bb052a5e81a209cdb167b2e8f
F92 <894465403@qq.com>
2024-01-24 14:52:36 +0800

    traps not ready

--- kernel/printf.c
+++ kernel/printf.c
@@ -133,3 +133,16 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace()
+{
+  uint64 fp = r_fp();
+  while (PGROUNDUP(fp) - PGROUNDDOWN(fp) == PGSIZE)
+  {
+    uint64 address = *(uint64 *)(fp - 8);
+    printf("%p\n",address);
+    fp = *(uint64 *)(fp - 16);
+  }
+  
+}
